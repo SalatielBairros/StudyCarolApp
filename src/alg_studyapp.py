d1138e4ec4f21a252e90e81859efae4de1627ec7
Salatiel Bairros <salatiel.costa@totvs.com.br>
2024-02-19 10:23:14 -0300

    adding base endpoints

--- src/alg_studyapp.py
+++ src/alg_studyapp.py
@@ -2,9 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET', 'POST'])
+def base():
+    return 'Running!'
+
 @app.route('/hello_world')
 def hello_world():
     return "Hello World"
+
+@app.route(f'/statusz')
+def app_statusz():
+    return 'ok'
+
+@app.route(f'/healthz')
+def app_healthz():
+    return 'ok'
 
 def main():
     """Runs from command prompt
